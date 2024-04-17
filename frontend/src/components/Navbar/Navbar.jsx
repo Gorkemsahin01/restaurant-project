4dad7dd534b0596e115de3b9b96b574aa8bc4737
gorkemsahin <gorkemsahin012@gmail.com>
2024-04-17 18:54:27 +0300

    Müşterinin seçtiği ürünlerin sipariş kısmında gözükmesi

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import {Link} from 'react-router-dom'
+
 
 function Navbar() {
 
   const [menu, setMenu] = useState("home")
   return (
     <div className='navbar'>
-    <img src={assets.logo} alt="" className='logo' />
+   <Link to='/'> <img src={assets.logo} alt="" className='logo' /></Link>
       
       <ul className="navbar-menu">
-        <li onClick={()=> setMenu("home")} className={menu==="home"? "active":"" }>Anasayfa</li>
-        <li onClick={()=> setMenu("menu")} className={menu==="menu"? "active":""}>Menü</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"? "active":""}>Mobile-app</li>
-        <li onClick={()=> setMenu("contact")} className={menu==="contact"? "active":""}>İletişim</li>
+        <Link  onClick={()=> setMenu("home")} className={menu==="home"? "active":"" }>Anasayfa</Link>
+        <a href='#explore-menu' onClick={()=> setMenu("menu")} className={menu==="menu"? "active":""}>Menü</a>
+        <a href='#app-dowland' onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"? "active":""}>Mobile-app</a>
+        <a href='#footer' onClick={()=> setMenu("contact")} className={menu==="contact"? "active":""}>İletişim</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+         <Link to='/card'> <img src={assets.basket_icon} alt="" /></Link>
           <div className="dot"></div>
         </div>
         <button>Kayıt olun</button>
--- frontend/src/pages/Card/Card.jsx
+++ frontend/src/pages/Card/Card.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Card.css'
+import { StoreContext } from '../../context/StoreContext'
 
-function Card() {
+const Card = () => {
+
+  const{cardItems,food_list,removeFromCard}=useContext(StoreContext)
+
+
   return (
-    <div>Card</div>
+    <div className='card'>
+      <div className="card-item">
+        <div className="card-item-title">
+          <p>Resim</p>
+          <p>Ürün</p>
+          <p>Fiyat</p>
+          <p>Adet</p>
+          <p>Toplam</p>
+          <p>İptal et</p>
+        </div>
+        <br />
+        <hr />
+
+        {food_list.map((item,index)=>{
+          if(cardItems[item._id]>0){
+            return(
+              <div>
+              <div className='card-item-title card-item-item'>
+              <img src={item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.price}₺</p>
+              <p>{cardItems[item._id]}</p>
+              <p>{item.price*cardItems[item._id]}₺</p>
+              <p onClick={()=>removeFromCard(item._id)} className='cross'>X</p>
+               
+              </div>
+              <hr />
+              </div>
+            )
+          }
+        })}
+
+        <div className="card-bottom">
+          <div className="card-total">
+            <h2>Toplam Ücretiniz</h2>
+            <div>
+              <div className="card-total-details">
+                   <p>Toplam</p>
+                   <p>{0}</p>
+              </div>
+              <hr />
+              <div className="card-total-details">
+                <p>Delivery fee</p>
+                <p>{2}</p>
+              </div>
+              <hr />
+              <div className="card-total-details">
+                 <p>Toplam </p>
+                 <b>{0}</b>
+              </div>
+            
+            </div>
+            <button>proced to checkout
+
+            </button>
+          </div>
+          <div className='card-table'>
+            <div>
+              <p>Masa Numarası Giriniz</p>
+              <div className="cart-tablr-input">
+                <input type="text" placeholder='Masa Numarası' />
+                <button>Gönder</button>
+              </div>
+            </div>
+          </div>
+       
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
