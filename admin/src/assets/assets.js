2b9df0e35fb756faf617298b141bb0a03ea9515e
gorkemsahin <gorkemsahin012@gmail.com>
2024-05-07 20:21:21 +0300

    mongodb'ye verileri ekleme ve çıkarma işlemi

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,14 +4,16 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
+import gorkemicon from './Gorkem.png'
 
-export const assets ={
-    logo,
-    add_icon,
-    order_icon,
-    profile_image,
-    upload_area,
-    parcel_icon
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_image,
+  upload_area,
+  parcel_icon,
+  gorkemicon,
 }
 
 export const url = 'http://localhost:4000'
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <NavLink to="/add" className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Ekle</p>
+        </NavLink>
+        <NavLink to="/list" className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List İtems</p>
+        </NavLink>
+        <NavLink to="/orders" className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Siparişler</p>
+        </NavLink>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
