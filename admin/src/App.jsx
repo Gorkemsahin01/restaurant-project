47cec73ccc34c17dc6f952068871cc026a3f256e
gorkemsahin <gorkemsahin012@gmail.com>
2024-05-07 14:57:53 +0300

    admin paneli yapımı başlangıcı

---
+++ admin/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Sidebar/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
+
+export default App
